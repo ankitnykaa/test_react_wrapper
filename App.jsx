0ac5b1c87341b0964bb335da4cbfa23c958c9936
ankit.pandey <ankit.pandey@nykaa.com>
2017-08-23 17:05:21 +0530

    initCommit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+   componentDidMount() {
+     
+   }
+   render() {
+      return (
+         <div className="widget" data-id="599329b0fa13431c51f90363">
+           
+         </div>
+      );
+   }
+}
+
+export default App;
